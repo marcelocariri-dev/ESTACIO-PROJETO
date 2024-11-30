db27e6eb4efac53fc0ac390643a12c8886cbbb17
Marcelo Cariri <marcelocariri.dev@gmail.com>
2024-11-29 22:37:43 -0300

    STT

--- pages/Settingsscreen.js
+++ pages/Settingsscreen.js
@@ -4,20 +4,45 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function Settingsscreen ({navigation}) {
     return (
-<View style={styles.container}>
-<Text style={styles.Title}>setingsscreen</Text>
-</View>
+        <View style={styles.container}>
+        <Text style={styles.title}>Configuração</Text>
+        <Image source={require('../assets/file.png')} style={styles.image} /> {/*foto temporária*/}
+        <Text style={styles.subtitle}>Funções</Text>
+        <View style={styles.buttonContainer}>
+          <Button title="Institucional" />
+          <Button title="Relatórios" />
+          <Button title="Notificações" />
+          <Button title="Suporte" />
+          <Button title="Log out" color="red" />
+        </View>
+      </View>
     );
-}
-
-const styles = StyleSheet.create({
+  };
+  
+  const styles = StyleSheet.create({
     container: {
       flex: 1,
-    
       alignItems: 'center',
       justifyContent: 'center',
+      backgroundColor: '#F5FCFF',   
+  
     },
-Title: {
-fontSize: '22',
-font: 'bold'
-}},)
+    title: {
+      fontSize: 24,
+      fontWeight: 'bold',   
+  
+    },
+    image: {
+      width: 200,
+      height: 200,
+      borderRadius: 100,
+      marginBottom: 20,
+    },
+    subtitle: {
+      fontSize: 18,
+      marginBottom: 20,
+    },
+    buttonContainer: {
+      width: '80%',
+    },
+  });
